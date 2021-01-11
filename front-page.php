2986b41cc007a61aec3c259b86e063cb43e94fc3
K-Zimny <kenneth.zimny@gmail.com>
2021-01-11 15:50:14 -0600

    Initial Commit

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+    <!-- <link rel="stylesheet" href="front-page.css"> -->
+    <?php wp_enqueue_style( 'style', get_stylesheet_uri() ); ?>
+</head>
+    <body>
+        <h1>Dave's Emporium</h1>
+    </body>
+</html>
