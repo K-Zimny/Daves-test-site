bf844993747f6e21291f981c5060f26ad465e61c
K-Zimny <kenneth.zimny@gmail.com>
2021-01-15 00:52:53 -0600

    Edited front-page and style.css

--- front-page.php
+++ front-page.php
@@ -13,11 +13,21 @@ get_header(); ?>
 
 <section class="home-page">
 	<div class="main-content">
-		<div class="content">
+		<div class="mission">
 			<img src="http://localhost/daves-test-site/wp-content/uploads/2021/01/daves-emporium-logo.png" alt="daves-emporium-logo"/>
 			<h1>Canna Accessories and Seeds Hand Selected for Patients, By Patients</h1>
-			
 		</div>
+		<div id="shop-content">
+			<div class="shop-content-flex-item">
+				<p>Place holder text</p>
+			</div>	
+			<div class="shop-content-flex-item">
+				<p>Place holder text</p>
+			</div>	
+			<div class="shop-content-flex-item">
+				<p>Place holder text</p>
+			</div>	
+		</div>	
 	</div>
 </section>
 
